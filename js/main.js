b5b68afb1302d4c7c3d06484f39b04282e279b7b
Thiago Moreira <thiago.cmoreira@yahoo.com.br>
2017-09-20 12:23:12 -0300

    :art: Adding enhancements and animation to the card and observation message

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-$('.ui.dropdown').dropdown()
+$('.ui.dropdown')
+  .dropdown()
+
+$('.special.cards .image')
+  .dimmer({
+    on: 'hover'
+  })
+
+$('.ui.rating')
+  .rating()
+
+$('.message .close')
+  .on('click', function() {
+    $(this)
+      .closest('.message')
+      .transition('fade')
+  })
