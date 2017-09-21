756867b6424d1e41efef9438b63adcd93a1ae320
Thiago Moreira <thiago.cmoreira@yahoo.com.br>
2017-09-21 14:49:55 -0300

    :art: Testing method to populate cards with axios data

--- api/product.js
+++ api/product.js
@@ -1,15 +1,33 @@
 axios.get('https://instabuy.com.br/apiv2_2/product.json?subcategory_id=57eec92f072d415b67c24175')
   .then((response) => {
     let { data } = response.data
-    console.log(data[0])
-
-    let urlImage = 'https://s3-us-west-2.amazonaws.com/ib.image.medium/m-' + data[0].thumb
-    document.querySelector('.label_image').src = urlImage
-    document.querySelector('.label_name').innerHTML = data[0].name
-    document.querySelector('.label_brand').innerHTML = data[0].brand
-    document.querySelector('.label_desc').innerHTML = data[0].description
-    document.querySelector('.label_price').innerHTML = 'R$ ' + data[0].pc[0].price    
+    data.splice(3, 8)
+    // createCard()
+    let cards = collectCards()
+    populateCards(cards, data)
   })
   .catch((error) => {
     console.log(error)
   })
+
+function collectCards() {
+  let cards = document.querySelectorAll('.card')
+  return cards
+}
+
+// function createCard() {
+//   console.log('Passei aqui')
+//   $("<p id='test'>My <em>new</em> text</p>").appendTo('body')
+// }
+
+function populateCards(cards, products) {
+  for(let product of products) {
+    let index = products.indexOf(product)
+
+    let urlImage = 'https://s3-us-west-2.amazonaws.com/ib.image.medium/m-' + product.thumb
+    cards[index].querySelector('.get-image').src = urlImage
+    cards[index].querySelector('.header.get-name').innerHTML = product.name
+    cards[index].querySelector('.get-brand').innerHTML = product.brand
+    cards[index].querySelector('.get-price').innerHTML = 'R$ ' + product.pc[0].price
+  }
+}
