cd4b53ccc0dbc22db064e251a5f67c44abbf4fc6
Thiago Moreira <thiago.cmoreira@yahoo.com.br>
2017-09-22 18:09:02 -0300

    :sparkles: Creating method to create card dynamically

--- api/product.js
+++ api/product.js
@@ -2,7 +2,7 @@ axios.get('https://instabuy.com.br/apiv2_2/product.json?subcategory_id=57eec92f0
   .then((response) => {
     let { data } = response.data
     data.splice(3, 8)
-    // createCard()
+    createCard()
     let cards = collectCards()
     populateCards(cards, data)
   })
@@ -15,10 +15,58 @@ function collectCards() {
   return cards
 }
 
-// function createCard() {
-//   console.log('Passei aqui')
-//   $("<p id='test'>My <em>new</em> text</p>").appendTo('body')
-// }
+function createCard() {
+  html = $('<div class="column">')
+    .append($('<div class="ui special cards">')
+      .append($('<div class="card">')
+        .append($('<div class="blurring dimmable image">')
+          .append($('<div class="ui dimmer">')
+            .append($('<div class="content">')
+              .append($('<div class="center">')
+                .append($('<div class="ui inverted button">')
+                  .append($('<i class="shop icon"></i>')
+                  .append('<span>Adicionar ao Carrinho</span>')
+                  )
+                )
+              )
+            )
+          )
+
+          .append($('<div class="ui fluid image label-product">')
+            .append($('<div class="ui green ribbon label">')
+              .append($('<i class="envira icon"></i>')
+              )
+              .append('Feira')
+            )
+          )
+          .append('<img class="get-image" src="https://www.thermofisher.com/blog/wp-content/uploads/2014/11/tomato.jpg">')
+        )
+
+        .append($('<div class="content">')
+          .append($('<a class="header get-name">Tomate</a>')
+          )        
+          .append($('<div class="meta">')
+            .append($('<span class="date get-brand">Pomar Brasília</span>')
+            )
+          )
+          .append($('<div class="description">')
+            .append($('<div class="ui rating" data-rating="4" data-max-rating="5">')
+            )
+          )
+        )                
+
+        .append($('<div class="extra content">')
+          .append($('<a class="price get-price">')
+            .append($('<span>R$ 14,90</span>')
+            )
+          )
+        )
+      )
+    )
+
+  $(".ui.three.column.grid.products").append(html)
+  $('.ui.rating').rating()
+}
 
 function populateCards(cards, products) {
   for(let product of products) {
